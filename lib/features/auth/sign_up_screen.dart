a78d951e5face63f531171d6f8d39556d58cc66a
WolfeLeo2 <muneneboizkaruga@gmail.com>
2026-07-01 14:32:25 +0300

    fix: random commit

--- lib/features/auth/sign_up_screen.dart
+++ lib/features/auth/sign_up_screen.dart
@@ -1,20 +1,21 @@
 import 'dart:io';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:intl_phone_field/intl_phone_field.dart';
+import 'package:path/path.dart' as path;
+import 'package:phosphor_flutter/phosphor_flutter.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:zynk/core/services/auth_service.dart';
 import 'package:zynk/core/utils/error_messages.dart';
 import 'package:zynk/features/auth/providers/lock_provider.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:path/path.dart' as path;
-import 'package:supabase_flutter/supabase_flutter.dart';
-
-import '../../core/theme/app_tokens.dart';
-import 'package:phosphor_flutter/phosphor_flutter.dart';
-import 'package:intl_phone_field/intl_phone_field.dart';
 import 'package:zynk/features/auth/widgets/auth_nav_button.dart';
 import 'package:zynk/features/auth/widgets/auth_progress_bar.dart';
+
+import '../../core/theme/app_tokens.dart';
 
 class SignUpScreen extends ConsumerStatefulWidget {
   const SignUpScreen({super.key});
@@ -118,7 +119,7 @@ class _SignUpScreenState extends ConsumerState<SignUpScreen> {
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(
             content: Text(
-              'Shop created! Check your email to confirm!',
+              'Shop created! Check your email to confirm.',
               style: TextStyle(
                 color: Theme.of(context).colorScheme.onSecondary,
               ),
